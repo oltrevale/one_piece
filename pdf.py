842bccf86017f94b440fe17d9cb845e18e01cb27
oltrevale <37437806+oltrevale@users.noreply.github.com>
2022-09-02 10:39:36 +0200

    Add files via upload

--- pdf.py
+++ pdf.py
@@ -11,28 +11,19 @@ def crea_pdf(start, end):
             Image.open(f"{capitolo}\\{indice}.jpg") for indice in range(lunghezza)
         ]
         images[0].save(
-            f"{capitolo}.pdf",
-            "PDF",
-            append_images=images[1:],save_all=True
+            f"{capitolo}.pdf", "PDF", append_images=images[1:], save_all=True
         )
-    merge = PdfMerger()
-    for capitolo in range(start, end + 1):
-        merge.append(PdfFileReader(f"{capitolo}.pdf"), "rb")
-    merge.write(f"prova_{start}-{end}.pdf")
-    writer = PdfFileWriter()  # open output
-    reader = PdfFileReader(f"prova_{start}-{end}.pdf")
-    indice = 0
-    pagine_totali = sum(dizionario.values())
-    print(pagine_totali)
-    writer = PdfFileWriter()
-    reader = PdfFileReader(f"prova_{start}-{end}.pdf")
-    for i in range(pagine_totali):
-        writer.addPage(reader.getPage(i))
-    for capitolo, lunghezza in dizionario.items():
+    writer=PdfFileWriter()
+    indice=0
+    for capitolo,lunghezza in dizionario.items():
+        reader=PdfFileReader(f'{capitolo}.pdf')
+        writer.addPage(reader.getPage(0))
         writer.addBookmark(f"{capitolo}", indice)
-        indice += lunghezza
+        indice+=lunghezza
+        for i in range(1,lunghezza):
+            writer.add_page(reader.getPage(i))
     with open(f"{start}-{end}.pdf", "wb") as f:
         writer.write(f)
 
 
-crea_pdf(1045, 1046)
+crea_pdf(1036, 1037)
