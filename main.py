79c643918558a7e77d461e5427cc5eaea8f269cc
oltrevale <37437806+oltrevale@users.noreply.github.com>
2022-09-16 18:01:26 +0200

    fix

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from tracemalloc import start
 import requests
 from bs4 import BeautifulSoup
+
 headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70"
-    }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70"
+}
+
 
 def return_links(chapter: int) -> list[str]:
-    lista=list_chapter_link()
-    link=lista[chapter-1] #zero-index-based
-    text = requests.get(link,headers=headers).text
+    lista = list_chapter_link()
+    link = lista[chapter - 1]  # zero-index-based
+    text = requests.get(link, headers=headers).text
     soup = BeautifulSoup(text, "html.parser")
     img = soup.find_all(class_="fixed-ratio-content")
-    return [im['src'] for im in img] 
-def list_chapter_link()->list[str]:
-    link = 'https://onepiecechapters.com/mangas/5/one-piece'
-    html=requests.get(link).text
-    soup=BeautifulSoup(html,'html.parser')
-    links=soup.find_all(class_='block border border-border bg-card mb-3 p-3 rounded')
-    links=[f"https://onepiecechapters.com{link['href']}" for link in links[::-1]]
-    links=[link for link in links if link[-2]!='.']
+    return [im["src"] for im in img]
+
+
+def list_chapter_link() -> list[str]:
+    link = "https://onepiecechapters.com/mangas/5/one-piece"
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all(class_="block border border-border bg-card mb-3 p-3 rounded")
+    links = [f"https://onepiecechapters.com{link['href']}" for link in links[::-1]]
+    links = [link for link in links if link[-2] != "."]
     return links
 
 
-def download_chapter(start, end=None) -> int|dict[int,int]:
+def download_chapter(start, end=None) -> dict[int, int] | int:
     import os
+
     if not end:
-        os.makedirs(f'{start}')
-        links=return_links(start)
+        os.makedirs(f"{start}")
+        links = return_links(start)
         for i, link in enumerate(links):
-                with open(f"{start}\\{i}.jpg", "wb") as f:
-                    f.write(requests.get(link).content)
+            with open(f"{start}\\{i}.jpg", "wb") as f:
+                f.write(requests.get(link).content)
         return len(links)
     else:
         dictionary = {}
         for chapter in range(start, end + 1):
-            os.makedirs(f'{chapter}')
+            os.makedirs(f"{chapter}")
             links = return_links(chapter)
             dictionary[chapter] = len(links)
             for i, link in enumerate(links):
--- pdf.py
+++ pdf.py
@@ -1,8 +1,9 @@
 import os
 from PIL import Image
-from PyPDF2 import  PdfReader,PdfMerger
+from PyPDF2 import PdfReader, PdfMerger
 import main
 import shutil
+
 
 def conv_rgba_to_rgb(file) -> Image.Image:
     rgba = Image.open(file)
@@ -11,32 +12,30 @@ def conv_rgba_to_rgb(file) -> Image.Image:
     return rgb
 
 
-
 def create_pdfs(start: int, end: int):
     dictionary = main.download_chapter(start, end)
-    merger=PdfMerger()
+    merger = PdfMerger()
     for chapter, lenght in dictionary.items():
-        images = [
-            conv_rgba_to_rgb(f"{chapter}\\{i}.jpg") for i in range(lenght)
-        ]
+        images = [conv_rgba_to_rgb(f"{chapter}\\{i}.jpg") for i in range(lenght)]
         images[0].save(f"{chapter}.pdf", "PDF", append_images=images[1:], save_all=True)
-        merger.append(f'{chapter}.pdf',f'{chapter}')
-    merger.write(f'{start}-{end}.pdf')
+        merger.append(f"{chapter}.pdf", f"{chapter}")
+    merger.write(f"{start}-{end}.pdf")
     merger.close()
     for chapter in dictionary.keys():
-        os.remove(f'{chapter}.pdf')
+        os.remove(f"{chapter}.pdf")
 
-def add_pdf(chapter:int,file:str):
-    lenght=main.download_chapter(chapter)
-    crea_pdf(chapter,lenght)
-    merger=PdfMerger()
-    reader = PdfReader(f'{file}.pdf')
+
+def add_pdf(chapter: int, file: str):
+    lenght = main.download_chapter(chapter)
+    images = [conv_rgba_to_rgb(f"{chapter}\\{i}.jpg") for i in range(lenght)]
+    images[0].save(f"{chapter}.pdf", "PDF", append_images=images[1:], save_all=True)
+    merger = PdfMerger()
+    reader = PdfReader(f"{file}.pdf")
     outlines = reader.getOutlines()
-    merger.append(f'{file}.pdf')
-    merger.append(f'{str(chapter)}.pdf',f'{chapter}')
+    merger.append(f"{file}.pdf")
+    merger.append(f"{str(chapter)}.pdf", f"{chapter}")
     for outline in outlines:
-        merger.add_bookmark(outline['/Title'],outline['/Page'])
-    merger.write(f'{file}+{str(chapter)}.pdf')
+        merger.add_bookmark(outline["/Title"], outline["/Page"])
+    merger.write(f"{file}+{str(chapter)}.pdf")
     merger.close()
-    shutil.rmtree(f'{str(chapter)}')
-
+    shutil.rmtree(f"{str(chapter)}")
